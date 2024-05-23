47385b1102da87af7615a8d7087e17a85db51e02
Cristian Ramirez Rodriguez <162833599+cristianramirezrodriguez@users.noreply.github.com>
2024-05-23 13:35:51 -0700

    Update FindSynodicPeriodFromSidereal.py

--- FindSynodicPeriodFromSidereal.py
+++ FindSynodicPeriodFromSidereal.py
@@ -8,10 +8,15 @@ def synodic(inferiorperiod, superiorperiod):
     S = 1 / (1 / T_i - 1 / T_s)
     return S
 
-print("The synodic period is the time between oppositions for two planets orbiting a star.")
-print("Earth's sidereal period is roughly 365 days.")
-Ti = float(input("The sidereal period of the inferior planet (days): "))
-Tf = float(input("The sidereal period of the superior planet (days): "))
+def main():
+    print("The synodic period is the time between oppositions for two planets orbiting a star.")
+    print("Earth's sidereal period is roughly 365 days.")
+    
+    Ti = float(input("The sidereal period of the inferior planet (days): "))
+    Tf = float(input("The sidereal period of the superior planet (days): "))
+    
+    Ts = synodic(Ti, Tf)
+    print("The synodic period is", Ts, "days.")
 
-Ts = synodic(Ti, Tf)
-print("The synodic period is", Ts, "days.")
+if __name__ == "__main__":
+    main()
